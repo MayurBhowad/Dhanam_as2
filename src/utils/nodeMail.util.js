21310065f70ddd7388abba930f09303d3c8669ca
MayurBhowad <mayurbhowad1995@gmail.com>
2021-12-04 23:09:51 +0530

    added: github workflow

--- src/utils/nodeMail.util.js
+++ src/utils/nodeMail.util.js
@@ -3,9 +3,8 @@ const nodemailer = require('nodemailer');
 const sendMail = (to_mail) => {
     return new Promise((resolve, reject) => {
         const transporter = nodemailer.createTransport({
+            service: 'gmail',
             host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
             auth: {
                 user: process.env.AUTH_EMAIL,
                 pass: process.env.AUTH_EMAIL_PASS
